b1042179db687c952bbd2e124efe02d366455dc7
DarleneCosta <darl_mel@hotmail.com>
2025-06-26 06:23:39 -0300

    Adicionar suporte ao TypeScript com a inclusão do pacote 'tsx', criar modelos e repositórios para postagens, além de configurar o VSCode para executar scripts TypeScript.

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,39 @@
+import { readFile } from 'fs/promises';
+import { PostModel } from '@/models/post/post-model';
+import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_PATH = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk(): Promise<PostModel[]> {
+    const file = await readFile(JSON_POSTS_PATH, 'utf8');
+    const { posts } = JSON.parse(file);
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    return await this.readFromDisk();
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.readFromDisk();
+    const post = posts.find(post => post.id === id);
+    if (!post) {
+      throw new Error('Post not found - 404');
+    }
+    return post;
+  }
+
+  async findBySlug(slug: string): Promise<PostModel> {
+    const posts = await this.readFromDisk();
+    const post = posts.find(post => post.slug === slug);
+    if (!post) {
+      throw new Error('Post not found - 404');
+    }
+    return post;
+  }
+}
+
+export const postRepository: PostRepository = new JsonPostRepository();
