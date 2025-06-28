a7c86c78712a60e0ff2514b29458bc333b65f047
DarleneCosta <darl_mel@hotmail.com>
2025-06-28 06:51:05 -0300

    Atualizar classes de estilo no componente Home para melhorar a responsividade e a interação visual da imagem ao passar o mouse.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ export default async function Home() {
     <Container>
       <Header />
 
-      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2'>
-        <Link className='w-full h-full overflow-hidden' href='#'>
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-lg' href='#'>
           <Image
             src='/images/bryen_0.png'
             alt='Post 1'
             width={1200}
             height={720}
-            className='hover:scale-105 transition-all duration-300'
+            className='group-hover:scale-105 transition-all duration-300'
           />
         </Link>
         <Suspense fallback={<SpinLoader />}>
