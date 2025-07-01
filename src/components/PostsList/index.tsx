55a17eeffe078fe9cdcff8db2288f6b2dc0e9e33
Darlene Costa <darl_mel@hotmail.com>
2025-07-01 16:00:20 -0300

    Atualizar componente PostsList para incluir uma grade responsiva com três colunas em telas médias, melhorando a apresentação visual dos posts.

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -5,7 +5,7 @@ import { PostHeading } from '../PostHeading';
 export const PostsList = async () => {
   const posts = await postRepository.findAll();
   return (
-    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3'>
       {posts.map(post => (
         <div key={post.id} className='flex flex-col gap-4 group'>
           <PostCoverImage
