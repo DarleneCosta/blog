925b3fa68cc13e6a789bcfaf6c789a57c25b7de1
DarleneCosta <darl_mel@hotmail.com>
2025-07-03 05:19:41 -0300

    Adicionar layout e estrutura ao componente RootLayout, incluindo cabeçalho e rodapé, além de atualizar metadados do blog. Criar nova página "About" com metadados apropriados. Remover componentes desnecessários da página inicial para simplificação.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import { Suspense } from 'react';
-import { Metadata } from 'next';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import { Container } from '@/components/Container';
-import { Header } from '@/components/Header';
 import { PostFeatured } from '@/components/PostFeatured';
-
-export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Home page',
-};
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -23,10 +13,6 @@ export default async function Home() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p>Copyright 2025</p>
-      </footer>
-    </Container>
+    </>
   );
 }
