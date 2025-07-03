620c0b0cf5981b242aaf2373f6a3937b290703a1
DarleneCosta <darl_mel@hotmail.com>
2025-07-03 06:03:42 -0300

    Adicionar título à página NotFound para melhorar a acessibilidade e a experiência do usuário ao exibir a mensagem de erro 404.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,22 +2,25 @@ import clsx from 'clsx';
 
 export default function NotFound() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px]',
-        'bg-slate-900',
-        'text-slave-100',
-        'mb-16 p-8 rounded-lg',
-        'flex flex-col items-center justify-center',
-        'text-center',
-        'dark:bg-slate-100',
-        'dark:text-slate-900',
-      )}
-    >
-      <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
-      <p className='text-slate-400 dark:text-slate-700'>
-        Erro 404 - A página que você está procurando não existe.
-      </p>
-    </div>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px]',
+          'bg-slate-900',
+          'text-slave-100',
+          'mb-16 p-8 rounded-lg',
+          'flex flex-col items-center justify-center',
+          'text-center',
+          'dark:bg-slate-100',
+          'dark:text-slate-900',
+        )}
+      >
+        <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
+        <p className='text-slate-400 dark:text-slate-700'>
+          Erro 404 - A página que você está procurando não existe.
+        </p>
+      </div>
+    </>
   );
 }
