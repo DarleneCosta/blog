32da681089ff933f98147eb7d162400c58eb7773
DarleneCosta <darl_mel@hotmail.com>
2025-07-22 05:33:52 -0300

    Adicionar funcionalidade de toggle no componente Menu, permitindo abrir e fechar o menu de navegação. Incluir ícones para melhorar a interface do usuário e utilizar a biblioteca clsx para gestão de classes CSS.

--- src/components/admin/Menu/index.tsx
+++ src/components/admin/Menu/index.tsx
@@ -1,12 +1,22 @@
+'use client';
+
 import clsx from 'clsx';
-import { FileTextIcon, HouseIcon } from 'lucide-react';
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon } from 'lucide-react';
+import { useState } from 'react';
 import Link from 'next/link';
 
 export function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const navClasses = clsx(
     'bg-slate-900 text-slate-100 rounded-md dark:bg-slate-100 dark:text-slate-900',
     'flex flex-col mb-8',
     'sm:flex-row sm:flex-wrap',
+    !isOpen && 'h-10',
   );
   const linkClasses = clsx(
     '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
@@ -15,8 +25,27 @@ export function Menu() {
     'h-10',
     'shrink-0',
   );
+  const buttonClasses = clsx(
+    linkClasses,
+    'text-blue-200 dark:text-blue-900 italic',
+  );
+
   return (
     <nav className={navClasses}>
+      <button onClick={toggleMenu} className={buttonClasses}>
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
       <a href='/' target='_blank' className={linkClasses}>
         <HouseIcon />
         Home
