06e2fc007272514163041c93155b47f70c11c3bc
DarleneCosta <darl_mel@hotmail.com>
2025-07-22 05:41:20 -0300

    Refatorar componente Menu para reiniciar o estado de abertura ao mudar de rota, utilizando useEffect e usePathname. Ajustar classes CSS para melhorar a responsividade e a aparência do menu.

--- src/components/admin/Menu/index.tsx
+++ src/components/admin/Menu/index.tsx
@@ -2,37 +2,41 @@
 
 import clsx from 'clsx';
 import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function Menu() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
   const navClasses = clsx(
     'bg-slate-900 text-slate-100 rounded-md dark:bg-slate-100 dark:text-slate-900',
     'flex flex-col mb-8',
     'sm:flex-row sm:flex-wrap',
     !isOpen && 'h-10',
+    'sm:h-auto',
   );
   const linkClasses = clsx(
     '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
     'flex items-center justify-start gap-2 rounded-md',
-    'transition hover:text-slate-800 dark:hover:text-slate-200',
+    'transition hover:text-slate-800 dark:hover:text-slate-300',
     'h-10',
     'shrink-0',
   );
   const buttonClasses = clsx(
     linkClasses,
     'text-blue-200 dark:text-blue-900 italic',
+    'sm:hidden',
   );
 
   return (
     <nav className={navClasses}>
-      <button onClick={toggleMenu} className={buttonClasses}>
+      <button onClick={() => setIsOpen(!isOpen)} className={buttonClasses}>
         {!isOpen && (
           <>
             <MenuIcon />
