3c45d2ef03b16ca48ca8e4473c5e93bb72328d66
DarleneCosta <darl_mel@hotmail.com>
2025-07-22 05:24:09 -0300

    Refatorar componente Menu para utilizar a biblioteca clsx, melhorando a gestão de classes CSS. Adicionar ícones ao menu de navegação para aprimorar a interface do usuário.

--- src/components/admin/Menu/index.tsx
+++ src/components/admin/Menu/index.tsx
@@ -1,12 +1,30 @@
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export function Menu() {
+  const navClasses = clsx(
+    'bg-slate-900 text-slate-100 rounded-md dark:bg-slate-100 dark:text-slate-900',
+    'flex flex-col mb-8',
+    'sm:flex-row sm:flex-wrap',
+  );
+  const linkClasses = clsx(
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+    'flex items-center justify-start gap-2 rounded-md',
+    'transition hover:text-slate-800 dark:hover:text-slate-200',
+    'h-10',
+    'shrink-0',
+  );
   return (
-    <nav className='flex flex-col gap-4'>
-      <a href='/' target='_blank'>
+    <nav className={navClasses}>
+      <a href='/' target='_blank' className={linkClasses}>
+        <HouseIcon />
         Home
       </a>
-      <Link href='/admin/posts'>Posts</Link>
+      <Link href='/admin/posts' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
