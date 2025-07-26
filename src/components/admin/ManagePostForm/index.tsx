a1031fb320053a4e953ab08930a88a5b4d8d446a
DarleneCosta <darl_mel@hotmail.com>
2025-07-26 07:15:03 -0300

    Atualizar o formulário de gerenciamento de postagens

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -12,15 +12,53 @@ export default function ManagePostForm() {
   return (
     <form className='flex flex-col gap-6 p-4'>
       <div className='flex flex-col gap-6 p-4'>
-        <InputCheckBox labelText='Título' />
-        <InputText labelText='Publicar' />
-        <ImageUploader />
+        <InputText
+          labelText='ID'
+          placeholder='ID gerado automaticamente'
+          readOnly
+          name='id'
+          type='text'
+        />
+        <InputText
+          labelText='Slug'
+          placeholder='Slug gerado automaticamente'
+          name='slug'
+          readOnly
+          type='text'
+        />
+        <InputText
+          labelText='Título'
+          name='title'
+          placeholder='Digite o título do post'
+          type='text'
+        />
+        <InputText
+          labelText='Autor'
+          name='author'
+          placeholder='Digite o nome do autor do post'
+          type='text'
+        />
+        <InputText
+          labelText='Resumo'
+          name='excerpt'
+          placeholder='Digite o resumo do post'
+          type='text'
+        />
         <MarkdownEditor
           labelText='Conteúdo'
           value={content}
           setValue={setContent}
           textAreaName='content'
         />
+        <ImageUploader />
+        <InputText
+          labelText='Url da imagem de capa'
+          name='coverImageUrl'
+          placeholder='Digite a url da imagem de capa'
+          type='text'
+        />
+
+        <InputCheckBox labelText='Publicar?' name='published' type='checkbox' />
         <div className='mt-4'>
           <Button type='submit'>Criar Post</Button>
         </div>
