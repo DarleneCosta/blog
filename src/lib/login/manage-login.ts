764afd42bf0e43b02c09017df4d38ded6eb5f882
DarleneCosta <darl_mel@hotmail.com>
2025-08-02 07:19:53 -0300

    Adicionar biblioteca 'jose' para manipulação de JWT e implementar função 'signJwt' para geração de tokens de sessão de login.

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import { cookies } from 'next/headers';
+import { SignJWT, jwtVerify } from 'jose';
 
 const jwtSecret = process.env.JWT_SECRET_KEY;
 const jwtEncodedKey = new TextEncoder().encode(jwtSecret);
@@ -7,6 +8,11 @@ const jwtEncodedKey = new TextEncoder().encode(jwtSecret);
 const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
 const loginExpStr = process.env.LOGIN_EXPIRATION_STRING || '1d';
 const loginCookieName = process.env.LOGIN_COOCKIE_NAME || 'loginSession';
+
+type JwtPayload = {
+  username: string;
+  expiredAt: Date;
+};
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10);
@@ -24,28 +30,32 @@ export async function verifyPassword(password: string, base64Hash: string) {
 }
 
 export async function createLoginSession(username: string) {
-  const expireAt = new Date(Date.now() + loginExpSeconds * 1000);
+  const expiredAt = new Date(Date.now() + loginExpSeconds * 1000);
 
-  const loginSession = username + 'quem sabe';
-  /*  jwt.sign({ username, expireAt }, jwtEncodedKey, {
-    expiresIn: loginExpStr,
-  });*/
+  const loginSession = await signJwt({ username, expiredAt });
 
   const cookieStore = await cookies();
 
   cookieStore.set(loginCookieName, loginSession, {
     httpOnly: true, //somente o servidor pode acessar o cookie
-    expires: expireAt,
+    expires: expiredAt,
     secure: true, //somente o servidor pode acessar o cookie
     sameSite: 'strict', //somente o servidor pode acessar o cookie
     path: '/',
   });
 
-  return { loginSession, expireAt };
+  return { loginSession, expiredAt };
 }
 
 export async function deleteLoginSession() {
   const cookieStore = await cookies();
   cookieStore.set(loginCookieName, '', { expires: new Date(0) });
   cookieStore.delete(loginCookieName);
+}
+export async function signJwt(jwtPayload: JwtPayload) {
+  return await new SignJWT(jwtPayload)
+    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+    .setIssuedAt()
+    .setExpirationTime(loginExpStr)
+    .sign(jwtEncodedKey);
 }
