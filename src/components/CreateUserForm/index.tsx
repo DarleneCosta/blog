8fcd8f73f63443b708a867c3f23546592f4c2b00
DarleneCosta <darl_mel@hotmail.com>
2025-09-19 05:39:18 -0300

    Adicionar ação de criação de usuário e esquema de validação. Atualizar componente CreateUserForm para integrar a nova ação e exibir mensagens de erro adequadas.

--- src/components/CreateUserForm/index.tsx
+++ src/components/CreateUserForm/index.tsx
@@ -1,10 +1,27 @@
-import clsx from 'clsx';
 import Link from 'next/link';
 import { UserRoundIcon } from 'lucide-react';
 import { Button } from '../Button';
+import { PublicUserSchema } from '@/lib/user/schemas';
+import { useActionState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { createUserAction } from '@/actions/user/create-user-action';
 import { InputText } from '../InputText';
+import clsx from 'clsx';
 
-export const CreateUserForm = () => {
+export function CreateUserForm() {
+  const [state, action, isPending] = useActionState(createUserAction, {
+    user: PublicUserSchema.parse({}),
+    errors: [],
+    success: false,
+  });
+
+  useEffect(() => {
+    toast.dismiss();
+    if (state.errors.length > 0) {
+      state.errors.forEach(error => toast.error(error));
+    }
+  }, [state]);
+
   return (
     <div
       className={clsx(
@@ -12,23 +29,23 @@ export const CreateUserForm = () => {
         'text-center max-w-sm mt-16 mb-32 mx-auto',
       )}
     >
-      <form className='flex-1 flex flex-col gap-6'>
+      <form action={action} className='flex-1 flex flex-col gap-6'>
         <InputText
           type='text'
           name='name'
           labelText='Nome'
           placeholder='Seu nome'
-          disabled={false}
-          defaultValue={''}
+          disabled={isPending}
+          defaultValue={state.user.name}
           required
         />
         <InputText
           type='email'
           name='email'
-          labelText='Email'
-          placeholder='Seu email'
-          disabled={false}
-          defaultValue={''}
+          labelText='E-mail'
+          placeholder='Sua e-mail'
+          disabled={isPending}
+          defaultValue={state.user.email}
           required
         />
         <InputText
@@ -36,25 +53,28 @@ export const CreateUserForm = () => {
           name='password'
           labelText='Senha'
           placeholder='Sua senha'
-          disabled={false}
+          disabled={isPending}
           required
         />
         <InputText
           type='password'
-          name='passwordConfirmation'
-          labelText='Confirmar senha'
+          name='password2'
+          labelText='Repetir senha'
           placeholder='Sua senha novamente'
-          disabled={false}
+          disabled={isPending}
           required
         />
-        <Button disabled={false} type='submit' className='mt-4'>
+
+        <Button disabled={isPending} type='submit' className='mt-4'>
           <UserRoundIcon />
-          Criar conta
+          {!isPending && 'Criar conta'}
+          {isPending && 'Criando...'}
         </Button>
+
         <p className='text-sm/tight'>
           <Link href='/login'>Já tem conta? Entrar</Link>
         </p>
       </form>
     </div>
   );
-};
+}
