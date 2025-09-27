1b28ec33fc34b97911fc4c6c30cbcf38a57dd841
DarleneCosta <darl_mel@hotmail.com>
2025-09-27 05:57:33 -0300

    Refatorar ação de exclusão de postagens para utilizar requisições API autenticadas. Atualizar a verificação de sessão de login e melhorar o tratamento de erros ao deletar postagens, garantindo que a resposta da API seja validada antes da exclusão.

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,37 +1,44 @@
 'use server';
 
-import { verifyLoginSession } from '@/lib/login/manage-login';
-import { postRepository } from '@/repositories/post';
+import { getLoginSessionForApi } from '@/lib/login/manage-login';
+import { PublicPostForApiDto } from '@/lib/posts/schemas';
+import { authenticatedApiRequest } from '@/utils/authenticate-api-request';
 import { revalidateTag } from 'next/cache';
 
 export async function deletePostAction(id: string) {
-  try {
-    const isAuthenticated = await verifyLoginSession();
-    if (!isAuthenticated) {
-      return { error: 'Faça login em outra aba antes de deletar o post' };
-    }
-    if (!id || typeof id !== 'string') {
-      return { error: 'Dados inválidos' };
-    }
-    const post = await postRepository.findById(id);
-    try {
-      await postRepository.deleteById(id);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        return {
-          error: error.message,
-        };
-      }
-      return {
-        error: 'Erro ao deletar post',
-      };
-    }
-
-    revalidateTag('posts');
-    revalidateTag(`posts-${post.slug}`);
-  } catch (error) {
-    return { error: error instanceof Error ? error.message : 'Unknown error' };
+  const isAuthenticated = await getLoginSessionForApi();
+  if (!isAuthenticated) {
+    return { error: 'Faça login em outra aba antes de deletar o post' };
+  }
+  if (!id || typeof id !== 'string') {
+    return { error: 'Dados inválidos' };
+  }
+  const postResponse = await authenticatedApiRequest<PublicPostForApiDto>(
+    `/posts/me/${id}`,
+    {
+      method: 'GET',
+    },
+  );
+  if (!postResponse.success) {
+    return { error: 'Post não encontrado' };
   }
 
-  return { error: null };
+  const deletePostResponse = await authenticatedApiRequest<PublicPostForApiDto>(
+    `/posts/me/${id}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  if (!deletePostResponse.success) {
+    return { error: 'Erro ao deletar post' };
+  }
+  const post = deletePostResponse.data;
+
+  revalidateTag('posts');
+  revalidateTag(`posts-${post.slug}`);
+
+  return { error: '' };
 }
